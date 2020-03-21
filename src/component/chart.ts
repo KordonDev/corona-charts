fa84a3782345e53e6320b51ea91d43a672c9189c
Arne Maier <Arne_Maier@gmx.de>
2020-03-21 17:04:33 +0100

    All relative dada

--- src/component/chart.ts
+++ src/component/chart.ts
@@ -1,29 +1,30 @@
 import Chart from 'chart.js';
 import { CountryData, Country } from '../services/util';
 
-export const createChart = (chartLabel: string, countriesData: Country[], dataType: Exclude<keyof CountryData, 'date'>, element: HTMLElement | null) => {
+export type DataMapper = (data: CountryData[]) => number[];
+
+export const createChart = (chartLabel: string, countriesData: Country[], dataMapper: DataMapper, element: HTMLElement | null) => {
     if (element instanceof HTMLCanvasElement) {
         const ctx = element.getContext('2d');
         if (ctx) {
-            new Chart(ctx, createConfig(chartLabel, countriesDataToChartData(countriesData, dataType)));
+            new Chart(ctx, createConfig(chartLabel, countriesDataToChartData(countriesData, dataMapper)));
         }
     }
 }
 
-const countriesDataToChartData = (countriesData: Country[], dataType: Exclude<keyof CountryData, 'date'>) => {
+const countriesDataToChartData = (countriesData: Country[], dataMapper: DataMapper) => {
     return {
         labels: getLabels(countriesData[0]),
         datasets: countriesData.map(country => ({
                 label: country.country,
                 backgroundColor: country.color,
                 borderColor: country.color,
-                data: country.data.map(day => day[dataType]),
+                data: dataMapper(country.data),
                 fill: false,
                 hidden: country.country === 'Germany' ? false : true,
         }))
     };
 }
-
 
 const getLabels = (country: Country) => country.data.map(day => `${day.date.getDate()}.${day.date.getMonth()+1}.`)
 
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,21 @@
 import { fetchCsvs } from './services/github.service';
-import { csvToJson } from './services/util';
+import { csvToJson, CountryData } from './services/util';
 
 import { createChart } from './component/chart';
+
+
+const confirmedDataMapper = (data: CountryData[]) => data.map(day => day.confirmed)
+const recoveredDataMapper = (data: CountryData[]) => data.map(day => day.recovered)
+const deathsDataMapper = (data: CountryData[]) => data.map(day => day.deaths)
+
+const confirmedRelativeDataMapper = (data: CountryData[]) => confirmedDataMapper(data).map(toRelativeData);
+const recoveredRelativeDataMapper = (data: CountryData[]) => recoveredDataMapper(data).map(toRelativeData);
+const deathsRelativeDataMapper = (data: CountryData[]) => deathsDataMapper(data).map(toRelativeData);
+
+const toRelativeData = (currentCases: number, index: number, cases: number[]) => {
+    if (index === 0 || cases[index - 1] === 0) return 0;
+    return Math.round(currentCases / cases[index - 1] * 1000)/1000;
+};
 
 fetchCsvs()
     .then(({ confirmedCsv, recoveredCsv, deathsCsv }) => csvToJson(confirmedCsv, recoveredCsv, deathsCsv))
@@ -10,7 +24,11 @@ fetchCsvs()
             .sort((country1, country2) => country1.data[country1.data.length - 1].confirmed - country2.data[country2.data.length - 1].confirmed)
             .slice(-20)
 
-        createChart('Confirmed Cases', top20ConfirmedCountries, 'confirmed', document.getElementById('absolute-confirmed'));
-        createChart('Recovered Cases', top20ConfirmedCountries, 'recovered', document.getElementById('absolute-recovered'));
-        createChart('Absolute Cases', top20ConfirmedCountries, 'deaths', document.getElementById('absolute-deaths'));
+        createChart('Confirmed Cases', top20ConfirmedCountries, confirmedDataMapper, document.getElementById('absolute-confirmed'));
+        createChart('Recovered Cases', top20ConfirmedCountries, recoveredDataMapper, document.getElementById('absolute-recovered'));
+        createChart('Absolute Cases', top20ConfirmedCountries, deathsDataMapper, document.getElementById('absolute-deaths'));
+
+        createChart('Confirmed Relative Changed', top20ConfirmedCountries, confirmedRelativeDataMapper, document.getElementById('change-rate-confirmed'));
+        createChart('Recovered Relative Changed', top20ConfirmedCountries, recoveredRelativeDataMapper, document.getElementById('change-rate-recovered'));
+        createChart('Deaths Relative Changed', top20ConfirmedCountries, deathsRelativeDataMapper, document.getElementById('change-rate-deaths'));
     });
