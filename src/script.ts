08a78e7dcf3a201b6465ea1293271ee85e7adcf0
Arne Maier <Arne_Maier@gmx.de>
2020-03-24 21:37:46 +0100

    Add active cases

--- src/script.ts
+++ src/script.ts
@@ -5,12 +5,15 @@ import { createChart } from './component/chart';
 
 
 const confirmedDataMapper = (data: CountryData[]) => data.map(day => day.confirmed)
-const recoveredDataMapper = (data: CountryData[]) => data.map(day => day.recovered)
 const deathsDataMapper = (data: CountryData[]) => data.map(day => day.deaths)
 
 const confirmedRelativeDataMapper = (data: CountryData[]) => confirmedDataMapper(data).map(toRelativeData);
 const deathsRelativeDataMapper = (data: CountryData[]) => deathsDataMapper(data).map(toRelativeData);
 
+const activeDays = 21;
+const activeDataMapper = (data: CountryData[]) => confirmedDataMapper(data).map(toActiveCases);
+
+const toActiveCases = (confirmed: number, index: number, confirmedCases: number[]) => index - activeDays < 0 ? confirmed : confirmed - confirmedCases[index - activeDays];
 const toRelativeData = (currentCases: number, index: number, cases: number[]) => {
     if (index === 0 || cases[index - 1] === 0) return 1;
     return Math.round(currentCases / cases[index - 1] * 1000)/1000;
@@ -32,4 +35,7 @@ fetchCsvs()
         createChart('Confirmed Relative Changed', top20ConfirmedCountries, confirmedRelativeDataMapper, document.getElementById('change-rate-confirmed-mobile'));
         createChart('Deaths Relative Changed', top20ConfirmedCountries, deathsRelativeDataMapper, document.getElementById('change-rate-deaths-desktop'));
         createChart('Deaths Relative Changed', top20ConfirmedCountries, deathsRelativeDataMapper, document.getElementById('change-rate-deaths-mobile'));
+
+        createChart('Active Cases', top20ConfirmedCountries, activeDataMapper, document.getElementById('active-cases-desktop'));
+        createChart('Active Cases', top20ConfirmedCountries, activeDataMapper, document.getElementById('active-cases-mobile'));
     });
