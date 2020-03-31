c669b338652d0260c34dfcce6490bc133296805a
Arne Maier <Arne_Maier@gmx.de>
2020-03-31 16:27:11 +0200

    Fetch files direct

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -1,35 +1,10 @@
 
-interface GitHubFileDescription {
-    name: string;
-    path: string;
-    sha: string;
-    size: number;
-    url: string;
-    html_url: string;
-    git_url: string;
-    download_url: string;
-    type: string;
-    _links: {
-        self: string;
-        git: string;
-        html: string;
-    }
-}
+const GLOBAL_CONFIRMED_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
+const GLOBAL_DEATHS_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
 
-const csvOrder = [ 'confirmed', 'deaths' ];
-const getCsvOrderIndex = (url: string) => csvOrder.findIndex(name => url.toLowerCase().includes(name));
 const fetchCsv = (url: string) => fetch(url).then(res => res.text())
 
-// Folder
-export const fetchCsvs = () => fetch('https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_time_series')
-    .then(response => response.json())
-    .then((files: GitHubFileDescription[]) => files
-        .filter(file => file.name.endsWith('.csv'))
-        .filter(file => file.name.startsWith('time_series_covid19') && csvOrder.some(csvName => file.name.includes(csvName)))
-        .map(file => file.download_url)
-        .sort((url1, url2) => getCsvOrderIndex(url1) - getCsvOrderIndex(url2))
-    )
-    .then(urls => Promise.all(urls.map(url => fetchCsv(url))))
-    .then(([ confirmedCsv, deathsCsv ]) => ({ confirmedCsv, deathsCsv }));
-
-
+export const fetchCsvs = () => {
+    return Promise.all([ GLOBAL_CONFIRMED_URL, GLOBAL_DEATHS_URL ].map(url => fetchCsv(url)))
+        .then(([ confirmedCsv, deathsCsv ]) => ({ confirmedCsv, deathsCsv }));
+}
